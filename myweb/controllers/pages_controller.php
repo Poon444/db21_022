781cf14e7ee03e5a5d49ae256c8fdf4e6dc44bc6
kaamonchanokk <91516215+kaamonchanokk@users.noreply.github.com>
2021-09-30 21:57:58 +0700

    update pages controller

---
+++ myweb/controllers/pages_controller.php
@@ -0,0 +1,13 @@
+<?php
+class PageContorller
+{
+    public function home()
+    {
+        require_once('views/page/home.php');
+    }
+    public function error()
+    {
+        require_once("views/page/home.php");
+    }
+}
+?>
