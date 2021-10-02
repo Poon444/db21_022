db7906030e8240292b385fd0c296dd648ba374a0
kaamonchanokk <91516215+kaamonchanokk@users.noreply.github.com>
2021-10-02 13:19:13 +0700

    Update del2

--- myweb/views/quotation/deleteConfirm.php
+++ myweb/views/quotation/deleteConfirm.php
@@ -1,11 +1,11 @@
-<?php echo "<br> คุณแน่ในนะว่าคุณจะลบ? <br>
-            <br> $quotaion->Q_ID $quotaion->CUS_Name <br>"; ?>
+<?php echo "<br> คุณแน่ใจนะว่าคุณจะลบใบเสนอราคา? <br>
+            <br> $quotation->Q_ID $quotation->CUS_NAME <br>"; ?>
 
 <form method="get" action="">
 
-    <input type="hidden" name="controller" value="quotaion" />
-    <input type="hidden" name="Q_ID" value="<?php echo $quotaion->Q_ID; ?>" />
-    <button type="submit" name="action" value="index">Back</button>
+    <input type="hidden" name="controller" value="quotation" />
+    <input type="hidden" name="Q_ID" value="<?php echo $quotation->Q_ID; ?>" />
+    <button type="submit" name="action" value="index">back</button>
     <button type="submit" name="action" value="delete">delete</button>
     
 
